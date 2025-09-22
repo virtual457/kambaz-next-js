3e9cc7381f2e4cbed65fe34fc548192b2b9564c3
Chandan <chandan.keelara@gmail.com>
2025-09-22 13:31:21 -0400

    Changed default username to Blast

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -3,7 +3,7 @@ export default function Signin() {
   return (
     <div id="wd-signin-screen">
       <h3>Sign in</h3>
-      <input placeholder="username" className="wd-username" defaultValue="alice" /> <br />
+      <input placeholder="username" className="wd-username" defaultValue="Blast" /> <br />
       <input placeholder="password" type="password" className="wd-password" defaultValue="123" /> <br />
       <Link href="/Dashboard" id="wd-signin-btn"> Sign in </Link> <br />
       <Link href="Signup" id="wd-signup-link"> Sign up </Link>
