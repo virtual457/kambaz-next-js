47fad3baee26449a5b341892e9ff1b4bb0c5e0bb
Chandan <chandan.keelara@gmail.com>
2025-10-03 01:45:21 -0400

    Update dependencies, integrate Bootstrap for styling, and refactor Kambaz layout components for improved structure and navigation

--- app/(Kambaz)/Navigation.tsx
+++ app/(Kambaz)/Navigation.tsx
@@ -1,14 +1,87 @@
+"use client";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function KambazNavigation() {
+  const pathname = usePathname();
+  const links = [
+    { label: "Dashboard", path: "/Dashboard", icon: AiOutlineDashboard },
+    { label: "Courses", path: "/Dashboard", icon: LiaBookSolid },
+    { label: "Calendar", path: "/Calendar", icon: IoCalendarOutline },
+    { label: "Inbox", path: "/Inbox", icon: FaInbox },
+    { label: "Labs", path: "/Labs", icon: LiaCogSolid },
+  ];
+
   return (
-    <div id="wd-kambaz-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
-      <Link href="/Account" id="wd-account-link">Account</Link><br/>
-      <Link href="/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link href="/Dashboard" id="wd-course-link">Courses</Link><br/>
-      <Link href="/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link href="/Inbox" id="wd-inbox-link">Inbox</Link><br/>
-      <Link href="/Labs" id="wd-labs-link">Labs</Link><br/>
-    </div>
+    <ListGroup 
+      id="wd-kambaz-navigation" 
+      style={{ width: 100 }}
+      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
+      
+      <ListGroupItem 
+        className="bg-black border-0 text-center" 
+        as="a"
+        target="_blank" 
+        href="https://www.northeastern.edu/" 
+        id="wd-neu-link">
+        <img src="/images/NEU.png" width="50px" alt="NEU" />
+      </ListGroupItem>
+      <br />
+
+      <ListGroupItem className="border-0 bg-black text-center">
+        <Link 
+          href="/Account" 
+          id="wd-account-link" 
+          className={`text-decoration-none ${
+            pathname.includes("Account") ? "text-danger" : "text-white"
+          }`}>
+          <FaRegCircleUser className={`fs-1 ${
+            pathname.includes("Account") ? "text-danger" : "text-white"
+          }`} />
+          <br />
+          Account
+        </Link>
+      </ListGroupItem>
+      <br />
+
+      <ListGroupItem 
+        as={Link}
+        href="/Dashboard"
+        id="wd-dashboard-link"
+        className={`border-0 text-center ${
+          pathname.includes("Dashboard") ? "bg-white" : "bg-black"
+        }`}>
+        <AiOutlineDashboard className="fs-1 text-danger" />
+        <br />
+        <span className={pathname.includes("Dashboard") ? "text-danger" : "text-white"}>
+          Dashboard
+        </span>
+      </ListGroupItem>
+      <br />
+
+      {links.slice(1).map((link) => (
+        <>
+          <ListGroupItem 
+            key={link.path}
+            as={Link}
+            href={link.path}
+            className={`border-0 text-center ${
+              pathname.includes(link.label) ? "bg-white" : "bg-black"
+            }`}>
+            {link.icon({ className: "fs-1 text-danger" })}
+            <br />
+            <span className={pathname.includes(link.label) ? "text-danger" : "text-white"}>
+              {link.label}
+            </span>
+          </ListGroupItem>
+          <br />
+        </>
+      ))}
+    </ListGroup>
   );
 }
